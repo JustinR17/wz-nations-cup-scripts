9ad9842b8dcb0002dea21359594f3b3edb87075b
JustinR17 <JustinR17WZ@gmail.com>
2025-08-03 05:17:57 +0100

    Upgrade scripts after rpi4 upgrade

--- run_bot_startup.sh
+++ run_bot_startup.sh
@@ -4,6 +4,5 @@ sleep 30
 pushd /home/pi/Desktop/nc
 tmux kill-session -t ncbot
 
-pyenv activate nc-venv
-tmux new -d -s ncbot 'python3 main.py bot'
+tmux new -d -s ncbot "/home/pi/.pyenv/versions/nc-venv/bin/python3 main.py bot"
 popd
--- run_parse_games.sh
+++ run_parse_games.sh
@@ -3,7 +3,6 @@
 source /home/pi/.bashrc
 pushd /home/pi/Desktop/nc
 printf '%s\n' "$(date)"
-pyenv activate nc-venv
-python3 main.py pgames
+/home/pi/.pyenv/versions/nc-venv/bin/python3 main.py pgames
 popd
 echo
